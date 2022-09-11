7f3456f1cb51829ce68d4617b81c75a2c89e954b
Mostafa Abdelhamid <mhsmustafa84@gmail.com>
2022-09-11 20:15:08 +0200

    fix

--- src/App.js
+++ src/App.js
@@ -9,8 +9,8 @@ function App() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path='/' element={<Navigate to='/react-recoil-app' />} />
-                <Route path='/react-recoil-app' element={<Outlet />}>
+                <Route path='' element={<Navigate to='react-recoil-app' />} />
+                <Route path='react-recoil-app' element={<Outlet />}>
                     <Route path='' element={<Products products={products} />} />
                     <Route path='add' element={<AddProducts setProducts={setProducts} />} />
                 </Route>
